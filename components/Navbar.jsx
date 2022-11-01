b2860d7c240bda4e039ebbd37f33056fcc6a520d
Mi <milad.parirokh@gmail.com>
2022-11-01 15:46:17 +0330

    navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full">
+      <ul className="w-full flex gap-12 navbarItem text-base">
+        <li className="active">خانه</li>
+        <li>ویدئو</li>
+        <li>تصویر</li>
+        <li>وبلاگ</li>
+        <li>درباره ما</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
