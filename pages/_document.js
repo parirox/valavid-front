130fd712beaf559f8ac918327266b60eb4e5922c
Mi <milad.parirokh@gmail.com>
2022-11-01 15:45:40 +0330

    extract document config

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="fa" dir='rtl'>
+                <Head>
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
