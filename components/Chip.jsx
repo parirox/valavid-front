41b353b988536eb84754ca15d3bda772c7bfc602
Mi <milad.parirokh@gmail.com>
2022-11-01 15:45:59 +0330

    chip component

---
+++ components/Chip.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const styles = {backgroundColor:"#ffffff44"}
+
+const Chip = ({icon,content}) => {
+    return (
+        <div className='rounded-full flex items-center gap-3 px-4 py-2' style={styles}>
+            <span className='w-4'>{icon}</span>
+            <p>{ content}</p>
+        </div>
+    );
+}
+
+export default Chip;
