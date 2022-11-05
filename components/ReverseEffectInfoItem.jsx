ba6c14848c0c1465583db08c19f2c7389aad47ce
mdyghayeni <mdyghayeni83@gmail.com>
2022-11-05 10:59:38 +0330

    resize reverse effect item component

--- components/ReverseEffectInfoItem.jsx
+++ components/ReverseEffectInfoItem.jsx
@@ -1,8 +1,8 @@
 const ReverseEffectInfoItem = ({ className, data, disable }) => {
     return (
         <>
-            <div className={`bg-neutral flex gap-5 rounded-full py-4 px-6 h-[7.25rem] items-center group ${disable ? '' : 'hover:bg-gradient-to-br hover:from-[#534CDA] hover:to-[#AEAAFF]'} ${className}`}>
-                <div className={`w-[5rem] h-[5rem] flex justify-center items-center btn-circle rounded-full ${disable ? 'bg-gray-700' : 'bg-primary group-hover:bg-white'}`}>
+            <div className={`bg-neutral flex gap-5 rounded-full py-4 px-6 h-[8.25rem] items-center group ${disable ? '' : 'hover:bg-gradient-to-br hover:from-[#534CDA] hover:to-[#AEAAFF]'} ${className}`}>
+                <div className={`w-[5.1rem] h-[5.1rem] flex justify-center items-center btn-circle rounded-full ${disable ? 'bg-gray-700' : 'bg-primary group-hover:bg-white'}`}>
                     {data.icon}
                 </div>
                 <div className="flex flex-col basis-auto">
