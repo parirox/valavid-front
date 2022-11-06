6ce9e13ed0e3fd10122a1fc28f357c52e3ddef93
Mi <milad.parirokh@gmail.com>
2022-11-07 00:05:27 +0330

    Collection Section

--- components/home/Collections.jsx
+++ components/home/Collections.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
-
+import CollectionGalleryCard from "@/components/CollectionGalleryCard";
+import { useState } from "react";
+import { IoFolderSharp } from "react-icons/io5";
 const Collections = () => {
-    return (
-        <div>
-            
+  const [images, setImages] = useState([]);
+
+  return (
+    <div className="container mx-auto px-56">
+      <div className="grid grid-cols-3 gap-x-14">
+        <div className="min-h-6">
+          <CollectionGalleryCard
+            items={images}
+            label="مجموعه زیبایی های ایرانی اسلامی"
+            icon={<IoFolderSharp />}
+          />
         </div>
-    );
-}
+        <div className="min-h-6">
+          <CollectionGalleryCard
+            items={images}
+            label="مجموعه زیبایی های ایرانی اسلامی"
+            icon={<IoFolderSharp />}
+          />
+        </div>
+        <div className="min-h-6">
+          <CollectionGalleryCard
+            items={images}
+            label="مجموعه زیبایی های ایرانی اسلامی"
+            icon={<IoFolderSharp />}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Collections;
