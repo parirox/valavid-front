bed1a72f52323c587c56f6b8b73b926a2464908a
mdyghayeni <mdyghayeni83@gmail.com>
2022-11-09 17:29:59 +0330

    title top section component : add padding bottom

--- components/SectionTitleDivider.jsx
+++ components/SectionTitleDivider.jsx
@@ -1,7 +1,7 @@
 import Rhomboid from '@/public/icons/FillRhomboidSmall.svg';
 export default function SectionTitleDivider({ title }) {
   return (
-    <div className="w-full items-center flex">
+    <div className="w-full items-center flex pb-20">
       <div className="border-b border-solid border-gray-700 basis-full"></div>
       <Rhomboid className="w-9"></Rhomboid>
       <h2 className="flex justify-center text-[1.75rem] min-w-fit px-14">{title}</h2>
