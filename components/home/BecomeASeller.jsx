4bc78f1db2cfe96015fdbd3c9976f334d7bc6101
Mi <milad.parirokh@gmail.com>
2022-11-16 19:47:47 +0330

    become a seller section

--- components/home/BecomeASeller.jsx
+++ components/home/BecomeASeller.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import Image from "next/image";
+import Button from '@/components/Button';
 
 const BecomeASeller = () => {
     return (
-        <div>
-            
+        <div className='mt-40 bg-accent h-80'>
+            <div className="container h-full">
+                <div className="flex h-full">
+                    <div className="basis-3/4 p-8">
+                        <div className="grid grid-row-5 h-full">
+                            <h3 className="font-bold">فروشنده شوید</h3>
+                            <p className="text-slate-200 text-xl">شما هم می توانید عضو شوید و تولیدات خود را به فروش برسانید</p>
+                            <div className="row-span-3 items-center flex">
+                                <Button className="rounded-3xl text-slate-50 bg-gradient-to-r from-[#9D99F8] to-[#534CDA] text-2xl w-1/4 h-20">فروشنده شوید</Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="basis-1/4 relative">
+                        <Image src={'/images/pink-camera.png'} className="absolute -top-9 left-0 object-cover" alt="" width={350} height={350} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
 
-export default BecomeASeller;
+export default BecomeASeller; 
--- tailwind.config.js
+++ tailwind.config.js
@@ -41,8 +41,12 @@ module.exports = {
     plugin(function ({ addBase, theme }) {
       addBase({
         html: { fontSize: "12px" },
-        h2: { fontSize: "40px" },
-        h5: { fontSize: "20px" },
+        h1: { fontSize: "3.5rem" },
+        h2: { fontSize: "3rem" },
+        h3: { fontSize: "2.5rem" },
+        h4: { fontSize: "2rem" },
+        h5: { fontSize: "1.5rem" },
+        h6: { fontSize: "1.2rem" },
       });
     }),
   ],
