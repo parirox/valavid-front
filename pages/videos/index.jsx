c3dd8c8d4f60cd6f00b5ca888717cec21871735c
mdyghayeni <mdyghayeni83@gmail.com>
2022-11-19 23:38:29 +0330

    add content video page

--- pages/videos/index.jsx
+++ pages/videos/index.jsx
@@ -2,6 +2,181 @@ import CoverPage from "@/components/CoverPage";
 import SortTabs from "@/components/SortTabs";
 import Head from "next/head";
 import VideoIcon from '@/public/icons/FillVideoPrimary.svg';
+import PopularCardVideo from "@/components/PopularCardVideo";
+
+const data = [
+  {
+    id: 1,
+    type: "video",
+    title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+    author: {
+      name: "حمید باقری",
+      profile_image: "https://placeimg.com/192/192/people",
+    },
+    price: "2,500 T",
+    stats: {
+      liked: false,
+      cart_added: false,
+      added_in_collection: false,
+    },
+    media: {
+      alt: "natural",
+      src: "/videos/sample1.mp4"
+    }
+  },
+  {
+    id: 4,
+    type: "video",
+    title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+    author: {
+      name: "حمید باقری",
+      profile_image: "https://placeimg.com/192/192/people",
+    },
+    price: "2,500 T",
+    stats: {
+      liked: false,
+      cart_added: false,
+      added_in_collection: false,
+    },
+    media: {
+      alt: "natural",
+      src: "/videos/sample2.mp4"
+    }
+  },
+  {
+    id: 1,
+    type: "video",
+    title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+    author: {
+      name: "حمید باقری",
+      profile_image: "https://placeimg.com/192/192/people",
+    },
+    price: "2,500 T",
+    stats: {
+      liked: false,
+      cart_added: false,
+      added_in_collection: false,
+    },
+    media: {
+      alt: "natural",
+      src: "/videos/sample1.mp4"
+    }
+  },
+  {
+    id: 4,
+    type: "video",
+    title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+    author: {
+      name: "حمید باقری",
+      profile_image: "https://placeimg.com/192/192/people",
+    },
+    price: "2,500 T",
+    stats: {
+      liked: false,
+      cart_added: false,
+      added_in_collection: false,
+    },
+    media: {
+      alt: "natural",
+      src: "/videos/sample2.mp4"
+    }
+  },
+  {
+    id: 1,
+    type: "video",
+    title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+    author: {
+      name: "حمید باقری",
+      profile_image: "https://placeimg.com/192/192/people",
+    },
+    price: "2,500 T",
+    stats: {
+      liked: false,
+      cart_added: false,
+      added_in_collection: false,
+    },
+    media: {
+      alt: "natural",
+      src: "/videos/sample1.mp4"
+    }
+  },
+  {
+    id: 4,
+    type: "video",
+    title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+    author: {
+      name: "حمید باقری",
+      profile_image: "https://placeimg.com/192/192/people",
+    },
+    price: "2,500 T",
+    stats: {
+      liked: false,
+      cart_added: false,
+      added_in_collection: false,
+    },
+    media: {
+      alt: "natural",
+      src: "/videos/sample2.mp4"
+    }
+  },
+  {
+    id: 1,
+    type: "video",
+    title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+    author: {
+      name: "حمید باقری",
+      profile_image: "https://placeimg.com/192/192/people",
+    },
+    price: "2,500 T",
+    stats: {
+      liked: false,
+      cart_added: false,
+      added_in_collection: false,
+    },
+    media: {
+      alt: "natural",
+      src: "/videos/sample1.mp4"
+    }
+  },
+  {
+    id: 1,
+    type: "video",
+    title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+    author: {
+      name: "حمید باقری",
+      profile_image: "https://placeimg.com/192/192/people",
+    },
+    price: "2,500 T",
+    stats: {
+      liked: false,
+      cart_added: false,
+      added_in_collection: false,
+    },
+    media: {
+      alt: "natural",
+      src: "/videos/sample1.mp4"
+    }
+  },
+  {
+    id: 4,
+    type: "video",
+    title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+    author: {
+      name: "حمید باقری",
+      profile_image: "https://placeimg.com/192/192/people",
+    },
+    price: "2,500 T",
+    stats: {
+      liked: false,
+      cart_added: false,
+      added_in_collection: false,
+    },
+    media: {
+      alt: "natural",
+      src: "/videos/sample2.mp4"
+    }
+  },
+]
 
 export default function index() {
   return (
@@ -13,8 +188,13 @@ export default function index() {
       <CoverPage className="bg-gradient-to-l from-[#13222db3] via-[#14232f59] to-[#13222db3]" value="مجموعه فیلم ویدئویی با کیفیت باورنکردنی" icon={<VideoIcon />} />
       <div className="flex w-full">
         <div className="basis-1/4 h-[40rem] bg-secondary-light"></div>
-        <div className="basis-full h-[40rem] px-10">
+        <div className="basis-full px-10 pb-[20rem]">
           <SortTabs></SortTabs>
+          <div className="flex flex-wrap py-7 w-full">
+              {data.map((video, key) => {
+                return <PopularCardVideo className="basis-1/3" key={key} data={video} />
+              })}
+          </div>
         </div>
       </div>
     </>
