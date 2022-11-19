19e916985945065ae7bcc5ccbd4f63796b6a4941
mdyghayeni <mdyghayeni83@gmail.com>
2022-11-19 14:36:10 +0330

    divider and resize blogCord

--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -4,14 +4,14 @@ import { IoCalendarClearOutline } from "react-icons/io5";
 export default function BlogCard({ className, data }) {
   return (
     <div className={`rounded-[3.875rem] group relative before:content-[''] overflow-hidden before:h-1/2 before:w-full before:absolute before:bottom-0 before:right-0 before:bg-gradient-to-b before:from-[#ffffff00] before:to-secondary before:opacity-70 ${className}`}>
-      <div className=" flex gap-1 absolute top-8 left-14">
+      <div className=" flex gap-1 absolute top-8 left-14 opacity-60 items-center text-sm">
         <IoCalendarClearOutline /> {data.date}
       </div>
-      <div className="w-full absolute bottom-0 right-0 px-20 pb-8">
+      <div className="w-full absolute bottom-0 right-0 px-14 pb-8">
         <div className="text-2xl">
           {data.title}
         </div>
-        <div className="w-32"></div>
+        <div className="w-32 border-t divide-solid my-4"></div>
         <div className="text-md font-light opacity-60">
           {data.description}
         </div>
