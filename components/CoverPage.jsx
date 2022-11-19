4409b4288afaef772d61324f1c910f2067aeab98
mdyghayeni <mdyghayeni83@gmail.com>
2022-11-19 22:56:09 +0330

    fix coverPage component

--- components/CoverPage.jsx
+++ components/CoverPage.jsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import PicMountain from '@/public/images/astara_mountain.jpg';
 import PageTitle from './PageTitle';
-import VideoIcon from '@/public/icons/FillVideoPrimary.svg';
 
-export default function CoverPage({ className }) {
+export default function CoverPage({ className, value, icon }) {
   return (
     <div className={`relative w-full flex items-center justify-center h-[30rem] ${className}`}>
       <Image fill sizes='' src={PicMountain} alt="" className='z-[-1]'></Image>
-      <PageTitle icon={<VideoIcon />}>مجموعه فیلم ویدئویی با کیفیت باورنکردنی</PageTitle>
+      <PageTitle icon={icon}>{value}</PageTitle>
     </div>
   )
 }
