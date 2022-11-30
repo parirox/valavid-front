dbc2880e47b3c6349afe938b3daf2735491bb0c4
Mi <milad.parirokh@gmail.com>
2022-11-30 18:02:29 +0330

    move viewport meta tag to the _app.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ function Home() {
     <>
       <Head>
         <title>Valavid</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Slider />
       <InfoItems />
