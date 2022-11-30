05492ea62f5363c5cd5e572382e646c31bcc567c
Mi <milad.parirokh@gmail.com>
2022-11-30 17:56:41 +0330

    move meta viewport in to the layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import Layout from "@/layouts/main/Layout";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout styleMode={Component?.styleMode}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
