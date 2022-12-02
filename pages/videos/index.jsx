d6efd09dd26e46f91b57dd63fa90619e9e789b9c
mdyghayeni <mdyghayeni83@gmail.com>
2022-12-02 12:35:25 +0330

    filter

--- pages/videos/index.jsx
+++ pages/videos/index.jsx
@@ -10,6 +10,7 @@ import { TiFilter } from "react-icons/ti"
 import { ImPriceTag } from "react-icons/im"
 import { IoIosArrowDown } from "react-icons/io"
 import RangeInput from "@/components/RangeInput";
+import { Disclosure, RadioGroup, Transition } from "@headlessui/react";
 
 
 const data = [
@@ -187,17 +188,33 @@ const data = [
 ]
 
 export default function Videos() {
-  const [isOpenPriceFilter, setIsOpenPriceFilter] = useState(false)
-  const [formData , setFormData] = useState({
+  const [formData, setFormData] = useState({
     price: 0,
+    resolution: 0,
+    frameRate: 0,
+    duration: 0,
+    setting: null,
+    space: null,
+    light: null,
+    colorTheme: null,
+    numberOfPeople: 0,
+    gender: null,
+    cameraAngle: null,
+    framing: null,
+    location: [null, null, null]
   })
+  const rates = [24, 28, 33]
+  const resolutions = ['همه', 'HD', '2K', '4K']
+  const settings = ['لوپ', 'آلفاچنل']
+  const spaces = ['باز', 'بسته']
+  const lights = ['روشن', 'تاریک']
 
-  const setFormDataHandler = (field) => (value)=> {
-    setFormData((prevState)=>({...prevState,[field]:value}))
+  const setFormDataHandler = (field) => (value) => {
+    setFormData((prevState) => ({ ...prevState, [field]: value }))
   }
-useEffect(() => {
-  console.log(formData);
-}, [formData])
+  useEffect(() => {
+    console.log(formData);
+  }, [formData])
 
   return (
     <>
@@ -207,7 +224,7 @@ useEffect(() => {
       </Head>
       <CoverPage className="bg-gradient-to-l from-[#13222db3] via-[#14232f59] to-[#13222db3]" value="مجموعه فیلم ویدئویی با کیفیت باورنکردنی" icon={<VideoIcon />} />
       <div className="flex w-full">
-        <div className="basis-1/4 h-[40rem] bg-secondary-light pt-8 px-7">
+        <div className="basis-1/4 bg-secondary-light pt-8 px-7">
           <Button
             className="h-14 w-40 rounded-2xl text-xl font-light bg-[#26333E] mr-2"
             icon={<TiFilter className="text-[2.1rem]" />}
@@ -215,20 +232,356 @@ useEffect(() => {
             فیلترها
           </Button>
           <div className="flex flex-col gap-14 pt-14">
-            <div className="border-b border-secondary-400 pr-2">
-              <div className="flex items-center justify-between gap-4 text-lg mr-3">
-                <div className="flex items-center gap-4">
-                  <ImPriceTag className="text-2xl" />
-                  قیمت
-                </div>
-                <button onClick={() => setIsOpenPriceFilter(!isOpenPriceFilter)}>
-                  <IoIosArrowDown className={`text-2xl text-secondary-200 mr-auto transition-all duration-500 ${isOpenPriceFilter ? 'rotate-180' : 'rotate-0'}`} />
-                </button>
-              </div>
-              <div className="w-full h-44 relative pt-20 px-3">
-                <RangeInput min={0} max={1000000} step={1000} state={formData.price} setState={setFormDataHandler('price')}></RangeInput>
-              </div>
-            </div>
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button>
+                    <div className="flex items-center justify-between gap-4 text-lg mr-3">
+                      <div className="flex items-center gap-4">
+                        <ImPriceTag className="text-2xl" />
+                        قیمت
+                      </div>
+                      <IoIosArrowDown className={`text-2xl text-secondary-200 mr-auto transition-all duration-500 ${open ? 'rotate-180' : 'rotate-0'}`} />
+
+                    </div>
+                  </Disclosure.Button>
+
+                  <Transition
+                    enter="transition duration-1000 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-250 ease-in-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0">
+                    <Disclosure.Panel className="border-b border-secondary-400 pr-2">
+                      <div className="w-full relative pt-10 py-24 px-3">
+                        <RangeInput min={0} max={1000000} step={1000} state={formData.price} setState={setFormDataHandler('price')}></RangeInput>
+                      </div>
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
+              )}
+
+            </Disclosure>
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button>
+                    <div className="flex items-center justify-between gap-4 text-lg mr-3">
+                      <div className="flex items-center gap-4">
+                        <ImPriceTag className="text-2xl" />
+                        رزولوشن
+                      </div>
+                      <IoIosArrowDown className={`text-2xl text-secondary-200 mr-auto transition-all duration-500 ${open ? 'rotate-180' : 'rotate-0'}`} />
+
+                    </div>
+                  </Disclosure.Button>
+
+                  <Transition
+                    enter="transition duration-1000 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-250 ease-in-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0">
+                    <Disclosure.Panel className="border-b border-secondary-400 pr-2">
+                      <div className="w-full relative pt-0 py-10 px-3">
+                        <RadioGroup value={formData.resolution} className="flex gap-4" onChange={setFormDataHandler('resolution')}>
+
+                          {resolutions.map(resolution => (
+                            <RadioGroup.Option value={resolution} key={resolution}
+                              className={({ active, checked }) =>
+                                `
+                                ${checked ? 'text-primary bg-white' : ''
+                                }
+                                w-fit flex justify-center items-center min-w-[5.5rem] px-4 py-2 rounded-xl border border-secondary-400 text-secondary-300`
+                              }
+                            >
+
+                              <button>{resolution}</button>
+
+                            </RadioGroup.Option>
+                          )
+                          )}
+                        </RadioGroup>
+                      </div>
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
+              )}
+
+            </Disclosure>
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button>
+                    <div className="flex items-center justify-between gap-4 text-lg mr-3">
+                      <div className="flex items-center gap-4">
+                        <ImPriceTag className="text-2xl" />
+                        فریم ریت
+                      </div>
+                      <IoIosArrowDown className={`text-2xl text-secondary-200 mr-auto transition-all duration-500 ${open ? 'rotate-180' : 'rotate-0'}`} />
+
+                    </div>
+                  </Disclosure.Button>
+
+                  <Transition
+                    enter="transition duration-1000 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-250 ease-in-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0">
+                    <Disclosure.Panel className="border-b border-secondary-400 pr-2">
+                      <div className="w-full relative pt-0 py-10 px-3">
+                        <RadioGroup value={formData.frameRate} className="flex gap-4" onChange={setFormDataHandler('frameRate')}>
+
+                          {rates.map(rate => (
+                            <RadioGroup.Option value={rate + ' fps'} key={rate}
+                              className={({ active, checked }) =>
+                                `
+                          ${checked ? 'text-primary bg-white' : ''
+                                }
+                          w-fit flex justify-center items-center min-w-[5.5rem] px-4 py-2 rounded-xl border border-secondary-400 text-secondary-300`
+                              }
+                            >
+
+                              <button>{rate + ' fps'}</button>
+
+                            </RadioGroup.Option>
+                          )
+                          )}
+                        </RadioGroup>
+                      </div>
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
+              )}
+
+            </Disclosure>
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button>
+                    <div className="flex items-center justify-between gap-4 text-lg mr-3">
+                      <div className="flex items-center gap-4">
+                        <ImPriceTag className="text-2xl" />
+                        زمان
+                      </div>
+                      <IoIosArrowDown className={`text-2xl text-secondary-200 mr-auto transition-all duration-500 ${open ? 'rotate-180' : 'rotate-0'}`} />
+
+                    </div>
+                  </Disclosure.Button>
+
+                  <Transition
+                    enter="transition duration-1000 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-250 ease-in-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0">
+                    <Disclosure.Panel className="border-b border-secondary-400 pr-2">
+                      <div className="w-full relative pt-10 py-24 px-3">
+                        <RangeInput min={0} max={60} unit="دقیقه" step={1} state={formData.duration} setState={setFormDataHandler('duration')}></RangeInput>
+                      </div>
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
+              )}
+
+            </Disclosure>
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button>
+                    <div className="flex items-center justify-between gap-4 text-lg mr-3">
+                      <div className="flex items-center gap-4">
+                        <ImPriceTag className="text-2xl" />
+                        تنظیمات
+                      </div>
+                      <IoIosArrowDown className={`text-2xl text-secondary-200 mr-auto transition-all duration-500 ${open ? 'rotate-180' : 'rotate-0'}`} />
+
+                    </div>
+                  </Disclosure.Button>
+
+                  <Transition
+                    enter="transition duration-1000 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-250 ease-in-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0">
+                    <Disclosure.Panel className="border-b border-secondary-400 pr-2">
+                      <div className="w-full relative pt-0 py-10 px-3">
+                        <RadioGroup value={formData.setting} className="flex gap-4" onChange={setFormDataHandler('setting')}>
+
+                          {settings.map(opt => (
+                            <RadioGroup.Option value={opt} key={opt}
+                              className={({ active, checked }) =>
+                                `
+                                ${checked ? 'text-primary bg-white' : ''
+                                }
+                                w-fit flex justify-center items-center min-w-[5.5rem] px-4 py-2 rounded-xl border border-secondary-400 text-secondary-300`
+                              }
+                            >
+
+                              <button>{opt}</button>
+
+                            </RadioGroup.Option>
+                          )
+                          )}
+                        </RadioGroup>
+                      </div>
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
+              )}
+
+            </Disclosure>
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button>
+                    <div className="flex items-center justify-between gap-4 text-lg mr-3">
+                      <div className="flex items-center gap-4">
+                        <ImPriceTag className="text-2xl" />
+                        فضا
+                      </div>
+                      <IoIosArrowDown className={`text-2xl text-secondary-200 mr-auto transition-all duration-500 ${open ? 'rotate-180' : 'rotate-0'}`} />
+
+                    </div>
+                  </Disclosure.Button>
+
+                  <Transition
+                    enter="transition duration-1000 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-250 ease-in-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0">
+                    <Disclosure.Panel className="border-b border-secondary-400 pr-2">
+                      <div className="w-full relative pt-0 py-10 px-3">
+                        <RadioGroup value={formData.space} className="flex gap-4" onChange={setFormDataHandler('space')}>
+
+                          {spaces.map(space => (
+                            <RadioGroup.Option value={space} key={space}
+                              className={({ active, checked }) =>
+                                `
+                                ${checked ? 'text-primary bg-white' : ''
+                                }
+                                w-fit flex justify-center items-center min-w-[5.5rem] px-4 py-2 rounded-xl border border-secondary-400 text-secondary-300`
+                              }
+                            >
+
+                              <button>{space}</button>
+
+                            </RadioGroup.Option>
+                          )
+                          )}
+                        </RadioGroup>
+                      </div>
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
+              )}
+
+            </Disclosure>
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button>
+                    <div className="flex items-center justify-between gap-4 text-lg mr-3">
+                      <div className="flex items-center gap-4">
+                        <ImPriceTag className="text-2xl" />
+                        نور
+                      </div>
+                      <IoIosArrowDown className={`text-2xl text-secondary-200 mr-auto transition-all duration-500 ${open ? 'rotate-180' : 'rotate-0'}`} />
+
+                    </div>
+                  </Disclosure.Button>
+
+                  <Transition
+                    enter="transition duration-1000 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-250 ease-in-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0">
+                    <Disclosure.Panel className="border-b border-secondary-400 pr-2">
+                      <div className="w-full relative pt-0 py-10 px-3">
+                        <RadioGroup value={formData.light} className="flex gap-4" onChange={setFormDataHandler('light')}>
+
+                          {lights.map(lightType => (
+                            <RadioGroup.Option value={lightType} key={lightType}
+                              className={({ active, checked }) =>
+                                `
+                                ${checked ? 'text-primary bg-white' : ''
+                                }
+                                w-fit flex justify-center items-center min-w-[5.5rem] px-4 py-2 rounded-xl border border-secondary-400 text-secondary-300`
+                              }
+                            >
+
+                              <button>{lightType}</button>
+
+                            </RadioGroup.Option>
+                          )
+                          )}
+                        </RadioGroup>
+                      </div>
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
+              )}
+
+            </Disclosure>
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button>
+                    <div className="flex items-center justify-between gap-4 text-lg mr-3">
+                      <div className="flex items-center gap-4">
+                        <ImPriceTag className="text-2xl" />
+                        تم رنگی
+                      </div>
+                      <IoIosArrowDown className={`text-2xl text-secondary-200 mr-auto transition-all duration-500 ${open ? 'rotate-180' : 'rotate-0'}`} />
+
+                    </div>
+                  </Disclosure.Button>
+
+                  <Transition
+                    enter="transition duration-1000 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-250 ease-in-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0">
+                    <Disclosure.Panel className="border-b border-secondary-400 pr-2">
+                      <div className="w-full relative pt-0 py-10 px-3">
+                        <RadioGroup value={formData.setting} className="flex gap-4" onChange={setFormDataHandler('setting')}>
+
+                          {settings.map(opt => (
+                            <RadioGroup.Option value={opt} key={opt}
+                              className={({ active, checked }) =>
+                                `
+                                ${checked ? 'text-primary bg-white' : ''
+                                }
+                                w-fit flex justify-center items-center min-w-[5.5rem] px-4 py-2 rounded-xl border border-secondary-400 text-secondary-300`
+                              }
+                            >
+
+                              <button>{opt}</button>
+
+                            </RadioGroup.Option>
+                          )
+                          )}
+                        </RadioGroup>
+                      </div>
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
+              )}
+
+            </Disclosure>
           </div>
         </div>
         <div className="basis-full px-10 pb-[20rem]">
