4b43c46e3c0e00d7e59b6e2e91c8edfc20dbae1a
mdyghayeni <mdyghayeni83@gmail.com>
2022-12-05 20:00:54 +0330

    fix cover page and page title component

--- components/CoverPage.jsx
+++ components/CoverPage.jsx
@@ -1,12 +1,11 @@
 import Image from 'next/image'
-import PicMountain from '@/public/images/astara_mountain.jpg';
 import PageTitle from './PageTitle';
 
-export default function CoverPage({ className, value, icon }) {
+export default function CoverPage({ className, children, icon, backgroundImage, description }) {
   return (
     <div className={`relative w-full flex items-center justify-center h-[30rem] ${className}`}>
-      <Image fill sizes='' src={PicMountain} alt="" className='z-[-1]'></Image>
-      <PageTitle icon={icon}>{value}</PageTitle>
+      <Image fill sizes='' src={backgroundImage} alt="" className='z-[-1]'></Image>
+      <PageTitle description={description} icon={icon}>{children}</PageTitle>
     </div>
   )
 }
--- components/PageTitle.jsx
+++ components/PageTitle.jsx
@@ -1,8 +1,9 @@
-export default function PageTitle({ icon, children }) {
+export default function PageTitle({ icon, children, description }) {
   return (
     <div className="flex items-center gap-3 flex-col">
       {icon ? <div className="bg-white flex justify-center items-center w-20 h-20 rounded-full">{icon}</div> : null}
       <h3> {children} </h3>
+      { description ? <p> { description } </p> : ''}
     </div>
   )
 }
