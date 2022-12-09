4bbc6c694e8986195f3d80c03bc1065265222143
mdyghayeni <mdyghayeni83@gmail.com>
2022-12-09 16:51:48 +0330

    advantage box component

---
+++ components/AdvantageBox.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function AdvantageBox({icon, advantage, description}) {
+  return (
+    <div className="flex gap-3">
+      <Image src={icon} alt="" width={50} className="w-14 h-[4rem]"></Image>
+      <div className="text-start">
+        <p className="text-lg">
+          {advantage}
+        </p>
+        <p className="text-base text-secondary-400 pt-2">
+          {description}
+        </p>
+      </div>
+    </div>
+  )
+}
