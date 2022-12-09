e4df434f78aff84c37b856c2c35d5099f8b27cfb
Mi <milad.parirokh@gmail.com>
2022-12-10 02:25:10 +0330

    add loading bar

--- layouts/main/Layout.jsx
+++ layouts/main/Layout.jsx
@@ -1,14 +1,16 @@
 import Footer from "@/layouts/main/Footer";
 import Header from "@/layouts/main/Header";
+import LoadingBar from 'react-redux-loading-bar'
 
-const Layout = ({ children, styleMode = 2 }) => {
-  return (
-    <>
-      <Header styleMode={styleMode} />
-      <main>{children}</main>
-      <Footer styleMode={styleMode} />
-    </>
-  );
+const Layout = ({children, styleMode = 2}) => {
+    return (
+        <>
+            <LoadingBar className={"bg-primary h-1 fixed z-50"} />
+            <Header styleMode={styleMode}/>
+            <main className={"min-h-screen"}>{children}</main>
+            <Footer styleMode={styleMode}/>
+        </>
+    );
 }
 
 
