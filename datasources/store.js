6d851fb2dde6ea8e7bd8df12243f4b31e5313e9f
Mi <milad.parirokh@gmail.com>
2022-12-09 16:54:02 +0330

    create redux store

---
+++ datasources/store.js
@@ -0,0 +1,44 @@
+import {configureStore} from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage'
+import {persistCombineReducers} from 'redux-persist'
+import {createWrapper} from 'next-redux-wrapper';
+
+//-->> checkout slices
+import checkoutSlice from '@/datasources/checkout/local/CheckoutSlice';
+import checkoutSliceApi, {checkoutSliceApiTag} from '@/datasources/checkout/remote/CheckoutSliceApi';
+//-->> product slices
+import productSlice from '@/datasources/product/local/ProductSlice';
+import productSliceApi, {productSliceApiTag} from '@/datasources/product/remote/ProductSliceApi';
+import {rtkQueryErrorLogger} from "@/datasources/errorHandler";
+
+const persistConfig = {
+    key: 'VALAVID',
+    timeout: 1000,
+    storage,
+    whitelist: ['checkout'],
+};
+
+let reducer = {
+    //->> checkout
+    checkout: checkoutSlice,
+    [checkoutSliceApiTag]: checkoutSliceApi.reducer,
+    //->> product
+    product: productSlice,
+    [productSliceApiTag]: productSliceApi.reducer,
+}
+reducer = persistCombineReducers(persistConfig, reducer);
+
+export const store = (context) => configureStore({
+    reducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }).concat([
+            rtkQueryErrorLogger,
+            checkoutSliceApi.middleware,
+            productSliceApi.middleware,
+        ]),
+})
+
+export const wrapper = createWrapper(store, {debug: false});
