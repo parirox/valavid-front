ee055320435360f67d50079796fc68ec89f8e65f
Mi <milad.parirokh@gmail.com>
2022-12-13 06:51:02 +0330

    selectable cart

--- components/PopularCardCollection.jsx
+++ components/PopularCardCollection.jsx
@@ -1,57 +1,63 @@
 import Image from 'next/image';
-import { BsFillDiamondFill } from 'react-icons/bs';
-import { IoFolderSharp } from 'react-icons/io5';
+import {BsFillDiamondFill} from 'react-icons/bs';
+import {IoFolderSharp} from 'react-icons/io5';
 import Badge from '@/components/Badge';
 import Link from 'next/link';
 
-const PopularCardCollection = ({ data, link = '#'}) => {
+const PopularCardCollection = ({data, link = '#'}) => {
     return (
         <div>
             <div className='h-[300px] group/popularCard relative'>
-                <div className="hidden group-hover/popularCard:block border-[1px] rounded-[3.35rem] w-full h-full z-30 absolute border-white">
-                    <BsFillDiamondFill className="w-9 absolute -left-[0.5px] top-2/4 -translate-y-2/4 -translate-x-2/4"></BsFillDiamondFill>
+                <div
+                    className="hidden group-hover/popularCard:block border-[1px] rounded-[3.35rem] w-full h-full z-30 absolute border-white">
+                    <BsFillDiamondFill
+                        className="w-9 absolute -left-[0.5px] top-2/4 -translate-y-2/4 -translate-x-2/4"></BsFillDiamondFill>
                 </div>
                 <div className="w-full h-full p-3">
-                    <div className="relative w-full h-full rounded-[2.6rem] overflow-hidden">
-                        <div className="absolute inset-0 z-40 transition-400-linear opacity-0 group-hover/popularCard:opacity-100 py-12 px-10 flex flex-col justify-between">
-                            <div className="basis-auto">
-                                <div className="flex p-1 gap-3 justify-between">
-                                    <div className="basis-auto">
-                                        <div className="flex gap-3 text-xl">
-                                            <Link href={link}>
+                    <Link href={link}>
+                        <div className="relative w-full h-full rounded-[2.6rem] overflow-hidden">
+                            <div
+                                className="absolute inset-0 z-40 transition-400-linear opacity-0 group-hover/popularCard:opacity-100 py-12 px-10 flex flex-col justify-between">
+                                <div className="basis-auto">
+                                    <div className="flex p-1 gap-3 justify-between">
+                                        <div className="basis-auto">
+                                            <div className="flex gap-3 text-xl">
                                                 <span>{data.label}</span>
-                                            </Link>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="basis-auto">
-                                        <div className="flex gap-3 justify-center items-center">
-                                            <Badge className='bg-primary rounded-2xl  text-2xl'><IoFolderSharp /></Badge>
+                                        <div className="basis-auto">
+                                            <div className="flex gap-3 justify-center items-center">
+                                                <Badge
+                                                    className='bg-primary rounded-2xl  text-2xl'><IoFolderSharp/></Badge>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="grid grid-rows-2 grid-cols-2 h-full gap-2 group/collection">
-                            {data.media.map((img, key) => (
-                                <div className="relative h-full" key={key}>
-                                    <div className="absolute inset-0 z-10 bg-gradient-to-bl group-hover/popularCard:from-[#00101C99] group-hover/popularCard:to-[#0E1F2C14]"></div>
-                                    <Image
-                                        src={img.src}
-                                        className="object-cover"
-                                        fill
-                                        sizes="(max-width: 768px) 100vw,
+                            <div className="grid grid-rows-2 grid-cols-2 h-full gap-2 group/collection">
+                                {data.media.map((img, key) => (
+                                    <div className="relative h-full" key={key}>
+                                        <div
+                                            className="absolute inset-0 z-10 bg-gradient-to-bl group-hover/popularCard:from-[#00101C99] group-hover/popularCard:to-[#0E1F2C14]"></div>
+                                        <Image
+                                            src={img.src}
+                                            className="object-cover"
+                                            fill
+                                            sizes="(max-width: 768px) 100vw,
                                         (max-width: 1200px) 50vw,
                                         33vw"
-                                        alt={img.alt}
-                                    />
-                                </div>
-                            ))}
+                                            alt={img.alt}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 </div>
             </div>
         </div>
-    );
+    )
+        ;
 }
 
 export default PopularCardCollection;
