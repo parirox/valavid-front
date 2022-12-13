17554fdd7e13f887c146d9ff3b489e5e6f1dc3c4
mdyghayeni <mdyghayeni83@gmail.com>
2022-12-13 13:02:08 +0330

    404 page

---
+++ pages/404/index.jsx
@@ -0,0 +1,25 @@
+import Button from "@/components/Button";
+import Head from "next/head";
+import Image from "next/image";
+
+export default function index() {
+  return (
+    <>
+      <Head>
+        <title>404 Error</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className="container relative min-h-[100vh] flex justify-center items-center">
+        <div className="h-[500px] w-[620px] relative">
+          <Image src={'/images/treePic404.png'} alt="" fill sizes=""></Image>
+        </div>
+        <div className="absolute text-[12rem] mt-28 mx-auto right-0 left-0 font-bold text-center">
+          404
+          <Button className={`text-xl w-64 mx-auto h-[4rem] -mt-7 rounded-xl transition-all bg-gradient-to-r from-[#9D99F8] to-[#534CDA]`} link={'/'}>
+            بازگشت به خانه
+          </Button>
+        </div>
+      </div>
+    </>
+  )
+}
