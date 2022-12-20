d3d1a3768a5036f632ea6b5bcb30261a7bb5ac46
mahdighayeni <mdyghayeni83@gmail.com>
2022-12-20 14:30:57 +0330

    connect to the API

--- components/home/InfoItems.jsx
+++ components/home/InfoItems.jsx
@@ -1,43 +1,21 @@
 import ReverseEffectInfoItem from '@/components/ReverseEffectInfoItem';
-import PicturIcon from '@/public/icons/FillPictureActive.svg';
-import VideoIcon from '@/public/icons/FillVideoActive.svg';
 import MusicIcon from '@/public/icons/FillMusicDisable.svg';
+import PicturIcon from '@/public/icons/FillPictureActive.svg';
 import ThemeIcon from '@/public/icons/FillThemeDisable.svg';
+import VideoIcon from '@/public/icons/FillVideoActive.svg';
 import OctagonalDivider from '../OctagonalDivider';
 
-export const InfoItemData = [
-    {
-        title: 'تصویر',
-        value: 1354,
-        icon: <PicturIcon className="scale-[0.7]" />
-    },
-    {
-        title: 'ویدئو',
-        value: 1354,
-        icon: <VideoIcon className="scale-[0.7]" />
-    },
-    {
-        title: 'صوت',
-        value: null,
-        icon: <MusicIcon className="scale-[0.7]" />
-    },
-    {
-        title: 'تمپلیت',
-        value: null,
-        icon: <ThemeIcon className="scale-[0.7]" />
-    },
-]
 
-const InfoItems = () => {
+const InfoItems = ({stats}) => {
     return (
         <div className="flex gap-16 max-w-[1120px] h-[20rem] items-center w-full m-auto">
-            <ReverseEffectInfoItem data={InfoItemData[0]} className="basis-1/4"></ReverseEffectInfoItem>
+            <ReverseEffectInfoItem title='تصویر' value={stats.images} icon={<PicturIcon className="scale-[0.7]" />} className="basis-1/4"></ReverseEffectInfoItem>
             <OctagonalDivider></OctagonalDivider>
-            <ReverseEffectInfoItem data={InfoItemData[1]} className="basis-1/4"></ReverseEffectInfoItem>
+            <ReverseEffectInfoItem title='ویدئو' value={stats.videos} icon={<VideoIcon className="scale-[0.7]" />} className="basis-1/4"></ReverseEffectInfoItem>
             <OctagonalDivider></OctagonalDivider>
-            <ReverseEffectInfoItem data={InfoItemData[2]} disable className="basis-1/4"></ReverseEffectInfoItem>
+            <ReverseEffectInfoItem title='صوت' value={stats.audios} icon={<MusicIcon className="scale-[0.7]" />} disable className="basis-1/4"></ReverseEffectInfoItem>
             <OctagonalDivider></OctagonalDivider>
-            <ReverseEffectInfoItem data={InfoItemData[3]} disable className="basis-1/4"></ReverseEffectInfoItem>
+            <ReverseEffectInfoItem title='تمپلیت' value={stats.templates} icon={<ThemeIcon className="scale-[0.7]" />} disable className="basis-1/4"></ReverseEffectInfoItem>
         </div>
     );
 }
