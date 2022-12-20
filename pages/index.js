84b6a088a07c28fabe284847af8c73fe5a04f20e
mahdighayeni <milad.parirokh@gmail.com>
2022-12-20 14:43:55 +0330

    connect to API

--- pages/index.js
+++ pages/index.js
@@ -6,29 +6,48 @@ import InfoItems from "@/components/home/InfoItems";
 import MostPopular from "@/components/home/MostPopular";
 import SubscribeBanner from "@/components/home/SubscribeBanner";
 import TopSellers from "@/components/home/TopSellers";
-import Slider from "@/components/Slider";
+import FirstSection from "@/components/home/FirstSection";
+import home_api, { GetHomeData, useGetHomeDataQuery } from "@/datasources/home/remote/HomeSliceApi";
+import { wrapper } from "@/datasources/store";
 import Head from "next/head";
+import { useEffect } from "react";
+import Error404 from "./404";
 
 function Home() {
-  return (
+  const { data, isSuccess, isError } = useGetHomeDataQuery();
+ 
+  if (isError) return <Error404 />
+
+  if (isSuccess) return (
     <>
       <Head>
-          <title>والاوید | صفحه ی اصلی</title>
+        <title>والاوید | صفحه ی اصلی</title>
       </Head>
-      <Slider />
+      <FirstSection video={data?.video} tags={data.tags} />
       {/* modal */}
       <CollectionModel />
-      <InfoItems />
-      <Collections />
-      <MostPopular />
-      <Blog />
+      <InfoItems stats={data.stats} />
+      <Collections data={data.collections} />
+      <MostPopular data={data.most_popular} />
+      <Blog data={data.blogs} />
       <SubscribeBanner />
-      <TopSellers />
+      <TopSellers data={data?.top_sellers} />
       <BecomeASeller />
     </>
   );
 }
 
 Home.styleMode = "main"
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async (context) => {
+    store.dispatch(GetHomeData.initiate())
+    await Promise.all(store.dispatch(home_api.util.getRunningQueriesThunk()))
+    return {
+      props: {},
+    };
+  }
+);
+
 
 export default Home;
