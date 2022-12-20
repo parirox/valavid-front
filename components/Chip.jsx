83cff255a672682945c37b53b2994ea162daeecf
mahdighayeni <mdyghayeni83@gmail.com>
2022-12-20 14:16:17 +0330

    add link and onClick handler params

--- components/Chip.jsx
+++ components/Chip.jsx
@@ -1,9 +1,21 @@
-const Chip = ({ icon, className, content }) => {
+import { isEmpty } from "@/utils/general";
+import Link from "next/link";
+
+const Chip = ({ icon, className, content, href, onClick = () => {} }) => {
+  const mergedClassName = "btn min-h-0 border-none rounded-full flex items-center gap-3 px-4 py-2 " + className;
+  if (isEmpty(href)) {
+    return (
+      <button className={mergedClassName} onClick={onClick}>
+        {icon}
+        <span>{content}</span>
+      </button>
+    )
+  }
   return (
-    <button className={"btn min-h-0 border-none rounded-full flex items-center gap-3 px-4 py-2 "+ className}>
+    <Link className={mergedClassName} href={href}>
       {icon}
       <span>{content}</span>
-    </button>
+    </Link>
   );
 };
 
