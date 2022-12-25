29371e7f4c175dbefc96453fde44234ddf858063
MiladParirokh <milad.parirokh@gmail.com>
2022-12-25 15:32:25 +0330

    set sample data due to uncompleted api

--- pages/footage/[id].jsx
+++ pages/footage/[id].jsx
@@ -25,11 +25,223 @@ import { MdRemoveShoppingCart } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import RatePieChart from "../../components/charts/RatePieChart";
 
+const data =  {
+    id: 1,
+    type: "video",
+    title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+    description: "سایت والاوید نخستین سایت ایرانی است که با تمرکز ویژه بر سبک زندگی ایرانیان، به روایت منظم آن پرداخته است . این سایت به دو زبان فارسی و انگلیسی هر آنچه را مورد نیاز در شناساندن حقیقی سیمای ایران و ایرانیان",
+    author: {
+        name: "حمید باقری",
+        profile_image: "https://placeimg.com/192/192/people",
+    },
+    publisher: {
+        id: 1,
+        name: "گروه تولیدی رسانه نوین"
+    },
+    price: {
+        main: "25,000",
+        off: "12,000",
+        percent: "40%"
+    },
+    stats: {
+        liked: false,
+        cart_added: false,
+        added_in_collection: false,
+    },
+    created_at: "1400/2/12",
+    media: {
+        alt: "natural",
+        src: "/videos/sample2.mp4"
+    },
+    tags: [
+        {
+            id: 1,
+            label: "مشهد"
+        },
+        {
+            id: 2,
+            label: "میلاد"
+        },
+        {
+            id: 3,
+            label: "برج میلاد"
+        },
+        {
+            id: 4,
+            label: "شب های قدر"
+        },
+        {
+            id: 5,
+            label: "امام رضا"
+        }
+    ],
+    extra_information: {
+        resolution: '4k',
+        codek: 'prores',
+        ratio: '16:9',
+        colors: ['#f55', '#f99', '#f1a'],
+        file_type: 'QuickTime',
+        frame_rate: '25 FPS',
+        time: '00:20',
+        file_size: '8.3 MB',
+        rates: [
+            {
+                total: 5,
+                rate: 3.5,
+                label: 'Framing'
+            },
+            {
+                total: 5,
+                rate: 3.5,
+                label: 'Beauty image'
+            },
+            {
+                total: 5,
+                rate: 3.5,
+                label: 'Pristine'
+            },
+            {
+                total: 5,
+                rate: 3.5,
+                label: 'Difficulty'
+            },
+            {
+                total: 5,
+                rate: 3.5,
+                label: 'Lighting'
+            },
+            {
+                total: 5,
+                rate: 3.5,
+                label: 'Decor'
+            },
+            {
+                total: 5,
+                rate: 3.5,
+                label: 'Color Correction'
+            },
+            {
+                total: 5,
+                rate: 3.5,
+                label: 'Admin point1'
+            },
+            {
+                total: 5,
+                rate: 3.5,
+                label: 'Admin point1'
+            },
+        ]
+    },
+    more_user_products: [
+        {
+            id: 1,
+            type: "image",
+            title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+            media: {
+                alt: "natural",
+                src: "https://placeimg.com/640/480/nature/1"
+            }
+        },
+        {
+            id: 2,
+            type: "image",
+            title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+            media: {
+                alt: "natural",
+                src: "https://placeimg.com/640/480/nature/2"
+            }
+        },
+        {
+            id: 3,
+            type: "image",
+            title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+            media: {
+                alt: "natural",
+                src: "https://placeimg.com/640/480/nature/3"
+            }
+        },
+        {
+            id: 4,
+            type: "image",
+            title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+            media: {
+                alt: "natural",
+                src: "https://placeimg.com/640/480/nature/4"
+            }
+        },
+    ],
+    related_products: [
+        {
+            id: 1,
+            type: "image",
+            title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+            media: {
+                alt: "natural",
+                src: "https://placeimg.com/640/480/nature/5"
+            }
+        },
+        {
+            id: 2,
+            type: "image",
+            title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+            media: {
+                alt: "natural",
+                src: "https://placeimg.com/640/480/nature/6"
+            }
+        },
+        {
+            id: 3,
+            type: "image",
+            title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+            media: {
+                alt: "natural",
+                src: "https://placeimg.com/640/480/nature/7"
+            }
+        },
+        {
+            id: 4,
+            type: "image",
+            title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+            media: {
+                alt: "natural",
+                src: "https://placeimg.com/640/480/nature/8"
+            }
+        },
+        {
+            id: 5,
+            type: "image",
+            title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+            media: {
+                alt: "natural",
+                src: "https://placeimg.com/640/480/nature/9"
+            }
+        },
+        {
+            id: 6,
+            type: "image",
+            title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+            media: {
+                alt: "natural",
+                src: "https://placeimg.com/640/480/nature/10"
+            }
+        },
+        {
+            id: 7,
+            type: "image",
+            title: "مقبره بزرگ زیبای شب در شهر اصفهان",
+            media: {
+                alt: "natural",
+                src: "https://placeimg.com/640/480/nature/12"
+            }
+        },
+    ]
+}
+
 function FootageDetails() {
     const dispatch = useDispatch();
     const router = useRouter();
 
-    const { data, isSuccess, isError } = useProductDetailsQuery(router.query);
+    const { data: data2, isSuccess, isError } = useProductDetailsQuery(router.query);
 
     const _cartItems = useSelector(cartItems);
     const is_in_cart = useMemo(() => {
@@ -42,9 +254,9 @@ function FootageDetails() {
     }, [data, _favoriteItems])
 
 
-    if (isError) return <Error404 />
+    if (!isError) return <Error404 />
 
-    if (isSuccess) {
+    if (!isSuccess) {
         return (
             <>
                 <Head>
