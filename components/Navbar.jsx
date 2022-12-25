a21aa41f365b234ff1bb53244d73f67c533ad444
MiladParirokh <milad.parirokh@gmail.com>
2022-12-25 15:28:18 +0330

    Checking the page path to activate the link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,17 @@
 
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 const Navbar = () => { 
+  const router = useRouter()
+
   return (
     <nav className="w-full flex gap-12 navbarItem text-base">
-        <Link href="/" className="active">خانه</Link>
-        <Link href="/videos">ویدئو</Link>
-        <Link href="#">تصویر</Link>
-        <Link href="#">وبلاگ</Link>
-        <Link href="/aboutUs">درباره ما</Link>
+        <Link className={router?.pathname === "/" ? "active" : ""} href="/">خانه</Link>
+        <Link className={router?.pathname === "/videos" ? "active" : ""} href="/videos">ویدئو</Link>
+        <Link className={router?.pathname === "/images" ? "active" : ""} href="/images">تصویر</Link>
+        <Link className={router?.pathname === "/blogs" ? "active" : ""} href="/blogs">وبلاگ</Link>
+        <Link className={router?.pathname === "/aboutUs" ? "active" : ""} href="/aboutUs">درباره ما</Link>
     </nav>
   );
 };
