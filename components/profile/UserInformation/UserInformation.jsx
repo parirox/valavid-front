89b5e1171ced2318cc913f5d2fef104b3a053c3f
MiladParirokh <milad.parirokh@gmail.com>
2023-01-03 17:50:51 +0330

    user information tab in profile

--- components/profile/UserInformation/UserInformation.jsx
+++ components/profile/UserInformation/UserInformation.jsx
@@ -1,36 +1,31 @@
 import Divider from "@/components/Divider";
 import { Disclosure } from "@headlessui/react";
-import DatePicker, { DateObject } from "react-multi-date-picker"
-import persian from "react-date-object/calendars/persian"
-import persian_fa from "react-date-object/locales/persian_fa"
-import "react-multi-date-picker/styles/backgrounds/bg-dark.css"
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
+import DatePicker, { DateObject } from "react-multi-date-picker";
+import "react-multi-date-picker/styles/backgrounds/bg-dark.css";
 
 import { FaChevronUp } from "react-icons/fa";
 
+import Button from "@/components/Button";
 import ButtonIcon from "@/components/ButtonIcon";
 import RadioButton from "@/components/Form/RadioButton";
+import { Fragment, useEffect, useMemo, useRef, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import { MdEdit } from "react-icons/md";
-import RowInput from "./RowInput";
-import CitySelect from "./CitySelect";
-import React, { Fragment, useEffect, useRef, useState } from "react";
-import SelectWithKey from "./SelectWithKey";
-import Button from "@/components/Button";
 import { IoAdd } from "react-icons/io5";
+import { MdEdit } from "react-icons/md";
+import CitySelect from "./CitySelect";
+import RowInput from "./RowInput";
+import SelectWithKey from "./SelectWithKey";
+import FileInput from "@/components/Form/FileInput";
+import { useRouter } from "next/router";
+import { isEmpty } from "@/utils/general";
+import Link from "next/link";
 
-const sections = [
-    {
-        name: "اطلاعات هویتی"
-    },
-    {
-        name: "فرم فروشنده"
-    },
-    {
-        name: "قبت تیم"
-    },
-]
 
 const UserInformation = () => {
+    const router = useRouter()
+
     const filterOptions = {
         genders: [
             {
@@ -43,9 +38,14 @@ const UserInformation = () => {
             }
         ],
     }
-    const [mySocials, setmysocials] = useState([{}])
 
-    const { register, control, getValues, handleSubmit, watch, formState: { errors } } = useForm({
+    const [activeSection, setActiveSection] = useState({
+        baseUserForm: false,
+        sellerForm: false,
+        teamForm: false,
+    });
+
+    const { register, control, setValue, getValues, handleSubmit, watch, formState: { errors } } = useForm({
         defaultValues: {
             firstname: '',
             lastname: '',
@@ -65,6 +65,8 @@ const UserInformation = () => {
             password: '',
             password_confimation: '',
             // third
+            email: '',
+            phonenumber: '',
             socials: [
                 {
                     name: 'instagram',
@@ -75,17 +77,62 @@ const UserInformation = () => {
                     address: ''
                 },
             ],
+            // forth
+            idcart_photo: '',
+            shabanumber: '',
+            banknumber: '',
+            // fifth
+            company_name: '',
+            company_address: '',
+            employees_count: '',
+            statute: '',
+            newspaper_ads: '',
+            // sixth
+            company_email: '',
+            company_phonenumber: '',
+            company_socials: [
+                {
+                    name: 'instagram',
+                    address: ''
+                },
+                {
+                    name: 'youtube',
+                    address: ''
+                },
+            ],
+            // seventh
+            company_idcart_photo: '',
+            company_shabanumber: '',
+            company_banknumber: '',
         }
     });
     const onSubmit = data => console.log(data);
-    const [selectedDay, setSelectedDay] = useState(null);
+
+    const addSocialSelect = (name) => () => {
+        setValue(`${name}.${getValues(name).length}`, {
+            name: 'instagram',
+            address: ''
+        })
+    }
+
+    useEffect(() => {
+        const sectionSelected = router.asPath.split('#')?.[1];
+        console.log(sectionSelected);
+        if (router.isReady && !isEmpty(sectionSelected)) {
+            if (document.querySelector(`.${sectionSelected}`)) {
+                document.querySelector(`.${sectionSelected}`).click()
+            };
+        }
+    }, [router.asPath])
 
 
     return (
-        <div>
-            <Disclosure defaultOpen={false}>
-                <Disclosure.Button className="py-2 w-full mt-16">
-                    <Divider className="text-secondary-300" start={"اطلاعات کاربری"} end={<FaChevronUp className="ui-open:rotate-180 ui-open:transform" />} />
+        <div className="mb-40">
+            <Disclosure>
+                <Disclosure.Button as={Link} scroll={false} href="#baseUserForm">
+                    <div id="baseUserForm" className="py-2 w-full mt-16 baseUserForm">
+                        <Divider className="text-secondary-300" start={"اطلاعات کاربری"} end={<FaChevronUp className="ui-open:rotate-180 ui-open:transform" />} />
+                    </div>
                 </Disclosure.Button>
                 <Disclosure.Panel className="text-lg py-5">
                     <form onSubmit={handleSubmit(onSubmit)}>
@@ -206,9 +253,11 @@ const UserInformation = () => {
                     </form>
                 </Disclosure.Panel>
             </Disclosure>
-            <Disclosure defaultOpen={true}>
-                <Disclosure.Button className="py-2 w-full mt-16">
-                    <Divider className="text-secondary-300" start={"فرم فروشنده"} end={<FaChevronUp className="ui-open:rotate-180 ui-open:transform" />} />
+            <Disclosure>
+                <Disclosure.Button as={Link} scroll={false} href="#sellerForm">
+                    <div id="sellerForm" className="py-2 w-full mt-16 sellerForm">
+                        <Divider className="text-secondary-300" start={"فرم فروشنده"} end={<FaChevronUp className="ui-open:rotate-180 ui-open:transform" />} />
+                    </div>
                 </Disclosure.Button>
                 <Disclosure.Panel className="text-lg py-5">
                     <form onSubmit={handleSubmit(onSubmit)}>
@@ -224,7 +273,7 @@ const UserInformation = () => {
                                     {...register('email')} />
                                 {errors.email && <span>This field is required</span>}
                             </RowInput>
-                            <RowInput label='شماره موبایل' required>
+                            <RowInput label='شماره موبایل' required helperText={'(باید به نام صاحب حساب باشد)'}>
                                 <input
                                     type="text"
                                     className="input-secondary w-2/6"
@@ -243,9 +292,9 @@ const UserInformation = () => {
                                             fieldState: { error }, //optional
                                             formState: { errors }, //optional, but necessary if you want to show an error message
                                         }) => (
-                                            value.map((social,k) => (
+                                            value.map((social, k) => (
                                                 <Fragment key={k}>
-                                                    <SelectWithKey value={social} setState={onChange} />
+                                                    <SelectWithKey value={social} index={k} setState={(newVal => setValue(`${name}.${k}`, newVal))} />
                                                     {
                                                         errors && errors[name] && errors[name].type === "required" && (
                                                             //if you want to show an error message
@@ -256,43 +305,131 @@ const UserInformation = () => {
                                             ))
                                         )}
                                     />
-                                    <Button onClick={() => setmysocials([])} className="btn-accent py-4"><IoAdd /></Button>
+                                    <Button onClick={addSocialSelect('socials')} className="btn-accent py-4"><IoAdd /></Button>
                                 </div>
                             </RowInput>
                         </div>
                     </form>
-                </Disclosure.Panel>
 
-
-                <Disclosure.Panel className="text-lg py-5">
-                    <form onSubmit={handleSubmit(onSubmit)}>
+                    <form onSubmit={handleSubmit(onSubmit)} className="mt-16">
                         <div className="w-full flex items-center gap-5">
                             <span className="flex-initial">بارگزاری مدارک</span>
                             <ButtonIcon icon={<MdEdit />} className="bg-secondary-400 text-2xl h-12 w-12" />
                         </div>
                         <div className="w-full text-gray-500 bg-secondary-light rounded-3xl border border-accent p-10 mt-8">
                             <RowInput label='تصویر کارت ملی /پاسپورت' required>
-                                <input type="file"
-                                    disabled
-                                    className="input-secondary w-2/6"
-                                    {...register('email')} />
-                                {errors.email && <span>This field is required</span>}
+                                <FileInput hookFormControl={control} name="idcart_photo" />
                             </RowInput>
-                            <RowInput label='شماره شبا' required>
-                                <input
-                                    type="text"
-                                    className="input-secondary w-2/6"
-                                    {...register('phonenumber')} />
+                            <RowInput label='شماره شبا' required helperText={'(باید به نام صاحب حساب باشد)'}>
+                                <input type="text" className="input-secondary w-2/6" {...register('shabanumber')} />
                                 {errors.phonenumber && <span>This field is required</span>}
                             </RowInput>
-                            <RowInput label='شماره حساب' required>
-                                <input
-                                    type="text"
-                                    className="input-secondary w-2/6"
-                                    {...register('phonenumber')} />
+                            <RowInput label='شماره حساب' required helperText={'(باید به نام صاحب حساب باشد)'}>
+                                <input type="text" className="input-secondary w-2/6" {...register('banknumber')} />
                                 {errors.phonenumber && <span>This field is required</span>}
                             </RowInput>
-                        
+
+                        </div>
+                    </form>
+                </Disclosure.Panel>
+            </Disclosure>
+
+            <Disclosure>
+                <Disclosure.Button as={Link} scroll={false} href="#teamForm">
+                    <div id="teamForm" className="py-2 w-full mt-16 sellerForm">
+                        <Divider className="text-secondary-300" start={"ثبت تیم"} end={<FaChevronUp className="ui-open:rotate-180 ui-open:transform" />} />
+                    </div>
+                </Disclosure.Button>
+                <Disclosure.Panel className="text-lg py-5">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="w-full flex items-center gap-5">
+                            <span className="flex-initial">اطلاعات شرکت</span>
+                            <ButtonIcon icon={<MdEdit />} className="bg-secondary-400 text-2xl h-12 w-12" />
+                        </div>
+                        <div className="w-full text-gray-500 bg-secondary-light rounded-3xl border border-accent p-10 mt-8">
+                            <RowInput label='نام شرکت'>
+                                <input type="text" className="input-secondary" {...register('company_name')} />
+                                {errors.firstname && <span>This field is required</span>}
+                            </RowInput>
+                            <RowInput label='آدرس شرکت'>
+                                <input type="text" className="input-secondary" {...register('company_address')} />
+                                {errors.lastname && <span>This field is required</span>}
+                            </RowInput>
+                            <RowInput label='تعداد کارمندان'>
+                                <input type="number" className="input-secondary w-auto"  {...register('employees_count')} />
+                                {errors.lastname && <span>This field is required</span>}
+                            </RowInput>
+                            <RowInput label='اساسنامه' required>
+                                <FileInput hookFormControl={control} name="statute" />
+                            </RowInput>
+                            <RowInput label='آگهی روزنامه' required>
+                                <FileInput hookFormControl={control} name="newspaper_ads" />
+                            </RowInput>
+                        </div>
+                    </form>
+
+                    <form onSubmit={handleSubmit(onSubmit)} className="mt-16">
+                        <div className="w-full flex items-center gap-5">
+                            <span className="flex-initial">اطلاعات تماس</span>
+                            <ButtonIcon icon={<MdEdit />} className="bg-secondary-400 text-2xl h-12 w-12" />
+                        </div>
+                        <div className="w-full text-gray-500 bg-secondary-light rounded-3xl border border-accent p-10 mt-8">
+                            <RowInput label='ایمیل'>
+                                <input type="text" className="input-secondary" {...register('company_email')} />
+                                {errors.firstname && <span>This field is required</span>}
+                            </RowInput>
+                            <RowInput label='تلفن'>
+                                <input type="text" className="input-secondary" {...register('company_phonenumber')} />
+                                {errors.lastname && <span>This field is required</span>}
+                            </RowInput>
+                            <RowInput label='شبکه های اجتماعی'>
+                                <div className="grid grid-cols-2 gap-10">
+                                    <Controller
+                                        control={control}
+                                        name="company_socials"
+                                        // rules={{ required: true }} //optional
+                                        render={({
+                                            field: { onChange, name, value },
+                                            fieldState: { error }, //optional
+                                            formState: { errors }, //optional, but necessary if you want to show an error message
+                                        }) => (
+                                            value.map((social, k) => (
+                                                <Fragment key={k}>
+                                                    <SelectWithKey value={social} index={k} setState={(newVal => setValue(`${name}.${k}`, newVal))} />
+                                                    {
+                                                        errors && errors[name] && errors[name].type === "required" && (
+                                                            //if you want to show an error message
+                                                            <span>your error message !</span>
+                                                        )
+                                                    }
+                                                </Fragment>
+                                            ))
+                                        )}
+                                    />
+                                    <Button onClick={addSocialSelect('company_socials')} className="btn-accent py-4"><IoAdd /></Button>
+                                </div>
+                            </RowInput>
+
+                        </div>
+                    </form>
+
+                    <form onSubmit={handleSubmit(onSubmit)} className="mt-16">
+                        <div className="w-full flex items-center gap-5">
+                            <span className="flex-initial">بارگزاری مدارک</span>
+                            <ButtonIcon icon={<MdEdit />} className="bg-secondary-400 text-2xl h-12 w-12" />
+                        </div>
+                        <div className="w-full text-gray-500 bg-secondary-light rounded-3xl border border-accent p-10 mt-8">
+                            <RowInput label='تصویر کارت ملی /پاسپورت' required>
+                                <FileInput hookFormControl={control} name="company_idcart_photo" />
+                            </RowInput>
+                            <RowInput label='شماره شبا' required helperText={'(باید به نام صاحب حساب باشد)'}>
+                                <input type="text" className="input-secondary w-2/6" {...register('company_shabanumber')} />
+                                {errors.phonenumber && <span>This field is required</span>}
+                            </RowInput>
+                            <RowInput label='شماره حساب' required helperText={'(باید به نام صاحب حساب باشد)'}>
+                                <input type="text" className="input-secondary w-2/6" {...register('company_banknumber')} />
+                                {errors.phonenumber && <span>This field is required</span>}
+                            </RowInput>
                         </div>
                     </form>
                 </Disclosure.Panel>
