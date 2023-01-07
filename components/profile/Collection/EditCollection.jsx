4be2a8a1a2b41d3b18e16591833709677fc739e3
Mi <milad.parirokh@gmail.com>
2023-01-07 13:47:04 +0330

    collection edit modal in profile

---
+++ components/profile/Collection/EditCollection.jsx
@@ -0,0 +1,110 @@
+import Button from "@/components/Button";
+import SingleCheckBox from "@/components/Form/SingleCheckBox";
+import Modal from "@/components/Modal";
+import {useEffect} from "react";
+import {IoClose, IoTrash} from "react-icons/io5";
+import ButtonIcon from "@/components/ButtonIcon";
+import {Controller, useForm} from "react-hook-form";
+import {useEditCollectionMutation, useRemoveCollectionMutation} from "@/datasources/user/remote/UserSliceApi";
+import toast from "@/utils/notification/toast";
+
+export default function EditCollection({show, showHandler, ...rest}) {
+    // const {data,isSuccess,isError,isLoading} = useAddCollectionMutation()
+    const [fetchEdit, {
+        data: editData,
+        isSuccess: editIsSuccess,
+        error: editError,
+        isError: editIsError,
+        isLoading: editIsLoading
+    }] = useEditCollectionMutation()
+    const [fetchRemove, {
+        data: removeData,
+        isSuccess: removeIsSuccess,
+        error: removeError,
+        isError: removeIsError,
+        isLoading: removeIsLoading
+    }] = useRemoveCollectionMutation()
+    const {id, is_published, title} = rest
+
+    const {register, control, reset, handleSubmit, formState: {errors}} = useForm()
+
+    const onSubmit = (data) => fetchEdit([{id}, data])
+
+    const removeCollectionHandler = () => {
+        fetchRemove({id})
+    };
+
+    //->> edit notify
+    useEffect(() => {
+        // on edit item
+        if (editIsSuccess) {
+            toast.success("ویرایش مجموعه با موفقیت انجام شد!")
+            showHandler(false)
+        }
+        if (editIsError) toast.error(editError)
+
+        // on edit remove
+        if (removeIsSuccess) {
+            toast.success("حذف مجموعه با موفقیت انجام شد!")
+            showHandler(false)
+        }
+        if (removeIsError) toast.error(removeError)
+    }, [editIsSuccess, editIsError, removeIsSuccess, removeIsError])
+
+    //->> initialize the default form value
+    useEffect(() => {
+        reset({
+            title: title,
+            is_public: is_published
+        })
+    }, [show])
+
+    return (
+        <Modal small isOpen={show} setIsOpen={(state) => showHandler(state)}>
+            <form onSubmit={handleSubmit(onSubmit)}
+                  className="bg-white w-full flex flex-col gap-8 rounded-3xl text-center p-10">
+                <div className="flex justify-between mb-5">
+                    <span className="text-secondary-300 text-xl">افزودن به مجموعه ها</span>
+                    <span className="text-accent text-2xl cursor-pointer"
+                          onClick={() => showHandler(false)}><IoClose/></span>
+                </div>
+                <div className="flex gap-14">
+                    <div className="relative w-full basis-4/6">
+                        <input {...register('title', {required: true})} type="text"
+                               className={`block full pb-2.5 border border-accent pr-4 rounded-3xl placeholder:text-accent text-accent border-gray-300 appearance-none peer ${errors.title && 'input-error'}`}
+                               placeholder="نام مجموعه را وارد کنید"/>
+                        <label
+                            className="absolute top-0 right-8 -translate-y-1/2 bg-white text-accent px-3 duration-200 transition-transform peer-placeholder-shown:opacity-0 peer-placeholder-shown:translate-y-1/2">نام
+                            مجموعه</label>
+                    </div>
+                    <div className="basis-2/6">
+                        <div className="flex gap-5">
+                            <Button className={"btn-accent flex-auto rounded-3xl px-10 py-6 text-xl"}
+                                    type={"submit"}>ویرایش</Button>
+                            <ButtonIcon className={"bg-gray h-20 w-20 flex-initial rounded-3xl text-3xl"}
+                                        onClick={removeCollectionHandler} icon={<IoTrash/>}/>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex gap-5 mb-5">
+                    <div className="relative w-full basis-4/6">
+                        <Controller
+                            control={control}
+                            name="is_public"
+                            render={({
+                                         field: {onChange, name, value},
+                                         fieldState: {error}, //optional
+                                         formState: {errors}, //optional, but necessary if you want to show an error message
+                                     }) => {
+                                return <>
+                                    <SingleCheckBox label='انتشار عمومی' selected={value} selectHandler={onChange}/>
+                                </>
+                            }
+                            }
+                        />
+                    </div>
+                </div>
+            </form>
+        </Modal>
+    )
+}
