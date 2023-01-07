533360a45985c0ea9c475cc90aeb51d1e7abf6f1
Mi <milad.parirokh@gmail.com>
2023-01-07 13:46:30 +0330

    connect to api

--- components/profile/Collection/Collections.jsx
+++ components/profile/Collection/Collections.jsx
@@ -1,102 +1,46 @@
 import CollectionGalleryCard from "@/components/CollectionGalleryCard";
-import { setModalCollectionTo } from "@/datasources/config/local/ConfigSlice";
-import { useState } from "react";
-import { CgFolderAdd } from "react-icons/cg";
-import { IoAdd, IoFolderSharp } from "react-icons/io5";
-import { useDispatch } from "react-redux";
+import {setModalCollectionTo} from "@/datasources/config/local/ConfigSlice";
+import {useState} from "react";
+import {IoAdd, IoFolderSharp} from "react-icons/io5";
+import {useDispatch} from "react-redux";
 import Button from "../../Button";
-import CollectionModal from "@/components/profile/Collection/CollectionModal";
-
-const data = [
-    {
-        id: 1,
-        title: 'مجموعه زیبایی های ایرانی اسلامی',
-        is_published: true,
-        total_count: 25,
-        media: [
-            {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            },
-            {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            }, {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            }, {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            }
-        ]
-    },
-    {
-        id: 2,
-        title: 'مجموعه زیبایی های ایرانی اسلامی',
-        is_published: false,
-        total_count: 25,
-        media: [
-            {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            },
-            {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            }, {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            }, {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            }
-        ]
-    },
-    {
-        id: 3,
-        title: 'مجموعه زیبایی های ایرانی اسلامی',
-        is_published: true,
-        total_count: 25,
-        media: [
-            {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            },
-            {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            }, {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            }, {
-                alt: "natural",
-                src: "https://placeimg.com/640/480/nature/1"
-            }
-        ]
-    },
-]
+import AddCollection from "@/components/profile/Collection/AddCollection";
+import EditCollection from "@/components/profile/Collection/EditCollection";
+import {useGetCollectionQuery} from "@/datasources/user/remote/UserSliceApi";
 
 
 const Collections = () => {
+    const {data, isSuccess, isError, isLoading} = useGetCollectionQuery()
+
     const dispatch = useDispatch()
+    const [show, setShow] = useState(false)
+    const [editData, setEditData] = useState({})
+    const editHandler = (data) => () => {
+        setEditData(data)
+        setShow(true)
+    }
+    if (!isSuccess) return <></>
     return (
         <div className="container">
-            <CollectionModal />
-            <Button className="btn-primary rounded-2xl px-8 py-4 mt-8 mb-16" onClick={() => dispatch(setModalCollectionTo({ active: true, footage_details: {} }))}>
-                <IoAdd className="text-2xl" />
+            {/*<AddCollection />*/}
+            <EditCollection show={show} showHandler={setShow} {...editData}/>
+            <AddCollection show={show} showHandler={setShow}/>
+            <Button className="btn-primary rounded-2xl px-8 py-4 mt-8 mb-16"
+                    onClick={() => setShow(true)}>
+                <IoAdd className="text-2xl"/>
                 <span>ایجاد مجموعه جدید</span>
             </Button>
             <div className="grid grid-cols-3 gap-x-14">
-                {data.map((collection, k) => (
+                {data.results.map((collection, k) => (
                     <div className="min-h-6" key={k}>
                         <CollectionGalleryCard
                             id={collection.id}
-                            items={collection.media}
+                            items={collection?.media}
                             label={collection.title}
-                            editLink={'collection/edit/' + collection.id}
+                            editHandler={editHandler(collection)}
                             is_published={collection.is_published}
                             total_count={collection.total_count}
-                            icon={<IoFolderSharp />}
+                            icon={<IoFolderSharp/>}
                         />
                     </div>
                 ))}
