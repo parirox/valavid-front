0bef297c18c830563b0a31f1166eb4d78288e13a
Mi <milad.parirokh@gmail.com>
2023-01-09 00:49:53 +0330

    refactoring

--- components/home/InfoItems.jsx
+++ components/home/InfoItems.jsx
@@ -1,6 +1,6 @@
 import ReverseEffectInfoItem from '@/components/ReverseEffectInfoItem';
 import MusicIcon from '@/public/icons/FillMusicDisable.svg';
-import PicturIcon from '@/public/icons/FillPictureActive.svg';
+import PictureIcon from '@/public/icons/FillPictureActive.svg';
 import ThemeIcon from '@/public/icons/FillThemeDisable.svg';
 import VideoIcon from '@/public/icons/FillVideoActive.svg';
 import OctagonalDivider from '../OctagonalDivider';
@@ -9,7 +9,7 @@ import OctagonalDivider from '../OctagonalDivider';
 const InfoItems = ({stats}) => {
     return (
         <div className="flex gap-16 max-w-[1120px] h-[20rem] items-center w-full m-auto">
-            <ReverseEffectInfoItem title='تصویر' value={stats.images} icon={<PicturIcon className="scale-[0.7]" />} className="basis-1/4"></ReverseEffectInfoItem>
+            <ReverseEffectInfoItem title='تصویر' value={stats.images} icon={<PictureIcon className="scale-[0.7]" />} className="basis-1/4"></ReverseEffectInfoItem>
             <OctagonalDivider></OctagonalDivider>
             <ReverseEffectInfoItem title='ویدئو' value={stats.videos} icon={<VideoIcon className="scale-[0.7]" />} className="basis-1/4"></ReverseEffectInfoItem>
             <OctagonalDivider></OctagonalDivider>
