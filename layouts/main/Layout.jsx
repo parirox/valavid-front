84a65fa0f4e446b47a5471811d4a95213c4ed239
Mi <milad.parirokh@gmail.com>
2023-01-08 12:22:42 +0330

    fix conflict

--- layouts/main/Layout.jsx
+++ layouts/main/Layout.jsx
@@ -1,17 +1,18 @@
 import Footer from "@/layouts/main/Footer";
 import Header from "@/layouts/main/Header";
-import LoadingBar from 'react-redux-loading-bar'
+import { useRouter } from "next/router";
+import LoadingBar from "react-redux-loading-bar";
 
-const Layout = ({children, styleMode = "others"}) => {
-    return (
-        <>
-            <LoadingBar className={"bg-primary h-1 fixed z-50"} />
-            <Header styleMode={styleMode}/>
-            <main className={`min-h-screen ${styleMode === 'blog' ? 'bg-[#F8F8F8]' : ''}`}>{children}</main>
-            <Footer styleMode={styleMode}/>
-        </>
-    );
-}
-
+const Layout = ({ children, styleMode = "others" }) => {
+  const router = useRouter();
+  return (
+    <>
+      <LoadingBar className={"bg-primary h-1 fixed z-50"}/>
+      {router.pathname !== "/auth" && <Header styleMode={styleMode}/>}
+      <main className={`min-h-screen ${styleMode === 'blog' ? 'bg-[#F8F8F8]' : ''}`}>{children}</main>
+      <Footer styleMode={styleMode}/>
+    </>
+  );
+};
 
 export default Layout;
