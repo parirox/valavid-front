d39ee52098e829d9f48ff2ac7106812133adfe64
Mi <milad.parirokh@gmail.com>
2023-01-16 13:30:39 +0330

    connect to api

--- components/profile/Favorites.jsx
+++ components/profile/Favorites.jsx
@@ -1,41 +1,21 @@
-import PopularCardVideo from "../PopularCardVideo";
-import PopularCardImage from '@/components/PopularCardImage';
-import {useGetCollectionQuery, useGetFavoritesQuery} from "@/datasources/user/remote/UserSliceApi";
+import {useGetFavoritesQuery} from "@/datasources/user/remote/UserSliceApi";
 import NoContent from "@/components/NoContent";
-
-const data = [
-  {
-    id: 6,
-    type: "image",
-    title: "مقبره بزرگ زیبای شب در شهر اصفهان",
-    author: {
-      name: "حمید باقری",
-      profile_image: "https://placeimg.com/192/192/people",
-    },
-    price: "2,500 T",
-    stats: {
-      liked: false,
-      cart_added: false,
-      added_in_collection: false,
-    },
-    media: {
-      alt: "natural",
-      src: "https://placeimg.com/640/480/nature"
-    }
-  },
-]
+import ProductCart from "@/components/ProductCart";
 
 const Favorites = () => {
-  const {data2, isSuccess, isError, isLoading} = useGetFavoritesQuery()
+  const {data, isSuccess, isError, isLoading} = useGetFavoritesQuery()
 
   if (!isSuccess) return <></>
   return (
-    <div className="flex flex-wrap py-7 w-full pb-48">
-      {data.count === 0 ? <NoContent/> :
-        data.map((favorite, key) => {
-
-        })
+    <div className="grid flex-wrap py-7 w-full pb-48">
+      {data.count === 0 && <NoContent/>}
+      <div className={`grid gap-2 grid-cols-3`}>
+      {
+        data.results.map((item, k) => (
+          <ProductCart link={`/products/${item.type}/${item.id}`} key={k} data={item}/>
+        ))
       }
+      </div>
     </div>
   );
 }
