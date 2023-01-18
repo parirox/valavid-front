750742c244b95dddb5515a834a946f664f134745
Mi <milad.parirokh@gmail.com>
2023-01-18 17:05:20 +0330

    connect to api

--- components/profile/Achievements.jsx
+++ components/profile/Achievements.jsx
@@ -1,45 +1,17 @@
-import MedalBox from "../MedalBox";
-import {useGetCartQuery, useGetMyAchievementsQuery} from "@/datasources/user/remote/UserSliceApi";
+import {useGetMyAchievementsQuery} from "@/datasources/user/remote/UserSliceApi";
 import NoContent from "@/components/NoContent";
-
-const MedalsData = [
-  {
-    title: "محبوبترین ویدئو هفته",
-    picture: "/images/mustPopularVideoMedal.png",
-    progressBar: 5
-  },
-  {
-    title: "1000+  لایک محصول",
-    picture: "/images/Medal2.png",
-    progressBar: 100
-  },
-  {
-    title: "محبوبترین ویدئو هفته",
-    picture: "/images/mustPopularVideoMedal.png",
-    progressBar: 100
-  },
-  {
-    title: "1000+  لایک محصول",
-    picture: "/images/Medal2.png",
-    progressBar: 70
-  },
-  {
-    title: "محبوبترین ویدئو هفته",
-    picture: "/images/mustPopularVideoMedal.png",
-    progressBar: 0
-  },
-]
+import MedalBox from "@/components/MedalBox";
 
 const Achievements = () => {
-  const {data, isSuccess, error, isError, isLoading} = useGetMyAchievementsQuery()
+  const {data, isSuccess} = useGetMyAchievementsQuery()
   if (!isSuccess) return <></>
   return (
     <div className="flex gap-12 flex-wrap pt-20">
       {data.count === 0 && <NoContent/>}
       {
-        MedalsData.map((medal, index) => (
-          <MedalBox className={'w-[calc(25%_-_2.26rem)]'} title={medal.title} picture={medal.picture}
-                    progressBar={medal.progressBar} key={index}></MedalBox>
+        data.results.map((medal, index) => (
+          <MedalBox className={'w-[calc(25%_-_2.26rem)]'} title={medal.title} picture={medal.image}
+                    progressBar={medal.progress} key={index}></MedalBox>
         ))
       }
     </div>
---
+++ components/publisher-profile/Achievements.jsx
@@ -0,0 +1,21 @@
+import {useGetPublisherAchievementsQuery} from "@/datasources/user/remote/UserSliceApi";
+import NoContent from "@/components/NoContent";
+import MedalBox from "@/components/MedalBox";
+
+const Achievements = () => {
+    const {data, isSuccess} = useGetPublisherAchievementsQuery()
+    if (!isSuccess) return <></>
+    return (
+      <div className="flex gap-12 flex-wrap pt-20">
+          {data.count === 0 && <NoContent/>}
+          {
+              data.results.map((medal, index) => (
+                <MedalBox className={'w-[calc(25%_-_2.26rem)]'} title={medal.title} picture={medal.image}
+                          progressBar={medal.progress} key={index}></MedalBox>
+              ))
+          }
+      </div>
+    );
+}
+
+export default Achievements;
