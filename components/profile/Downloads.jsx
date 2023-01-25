d785b1492c94e87fc268c92cc959d421500e9f93
Mi <milad.parirokh@gmail.com>
2023-01-25 16:24:58 +0330

    remove sample data

--- components/profile/Downloads.jsx
+++ components/profile/Downloads.jsx
@@ -3,44 +3,6 @@ import {useGetDownloadsQuery, useGetMyAchievementsQuery} from "@/datasources/use
 import NoContent from "@/components/NoContent";
 import MedalBox from "@/components/MedalBox";
 
-const DownloadsData = [
-    {
-        title: "تصویر سی و سه پل در شب مهتابی",
-        filters: [],
-        haveLicense: true,
-        price: 250000,
-        media: '/videos/sample2.mp4',
-        type: "video",
-        mediaLink: ''
-    },
-    {
-        title: "تصویر سی و سه پل در شب مهتابی",
-        filters: ["24 fps", "4K", "QuickTime","24 fps", "4K", "QuickTime","24 fps", "4K", "QuickTime","24 fps", "4K", "QuickTime",],
-        haveLicense: false,
-        price: 20000,
-        media: '/videos/sample2.mp4',
-        type: "video",
-        mediaLink: ''
-    },
-    {
-        title: "تصویر سی و سه پل در شب مهتابی",
-        filters: [],
-        haveLicense: true,
-        price: 250000,
-        media: "https://placeimg.com/640/480/nature/10",
-        type: "image",
-        mediaLink: ''
-    },
-    {
-        title: "تصویر سی و سه پل در شب مهتابی",
-        filters: ["24 fps", "4K",],
-        haveLicense: true,
-        price: null,
-        media: "/videos/sample2.mp4",
-        type: "video",
-        mediaLink: ''
-    },
-]
 
 const Downloads = () => {
     // const {data, isSuccess} = useGetDownloadsQuery()
