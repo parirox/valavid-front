fc10af4778c71e5ebe8d4fe02322e1fabcd6ce70
Mi <milad.parirokh@gmail.com>
2023-04-27 14:23:20 +0430

    bug fixes

--- components/ReverseEffectInfoItem.jsx
+++ components/ReverseEffectInfoItem.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import classNames from "classnames";
 
 const ReverseEffectInfoItem = ({ link,className, title, value, icon, disable }) => {
     return (
         <>
-            <div className={`bg-neutral flex gap-2 xl:gap-5 rounded-full py-0 xl:py-4 px-3 xl:px-6 h-[6.25rem] xl:h-[8.25rem] items-center group ${disable ? '' : 'hover:bg-gradient-to-br hover:from-[#534CDA] hover:to-[#AEAAFF] hover:text-accent relative'} ${className}`}>
+            <div className={classNames(`bg-neutral flex gap-2 xl:gap-5 rounded-full py-0 xl:py-4 px-3 xl:px-6 h-[6.25rem] xl:h-[8.25rem] items-center group ${disable ? '' : 'hover:bg-gradient-to-br hover:from-[#534CDA] hover:to-[#AEAAFF] hover:text-accent relative'} ${className}`)}>
                 { link && <Link href={link} className={"absolute inset-0"}></Link>}
                 <div className={`p-5 flex justify-center items-center btn-circle rounded-full ${disable ? 'bg-gray-700' : 'bg-primary group-hover:bg-white'}`}>
                     {icon}
--- components/home/InfoItems.jsx
+++ components/home/InfoItems.jsx
@@ -5,22 +5,22 @@ import { FaPalette } from "react-icons/fa";
 
 const InfoItems = ({ stats }) => {
   return (
-    <div className="px-6 flex flex-wrap sm:flex-nowrap justify-center gap-2 sm:gap-4 xl:gap-16 max-w-[1120px] h-[15rem] sm:h-[20rem] items-center w-full my-16 sm:my-auto m-auto">
+    <div className="px-6 flex flex-wrap sm:flex-nowrap justify-center gap-2 sm:gap-4 xl:gap-16 max-w-[1120px] h-[15rem] sm:h-[20rem] items-center w-full my-6 sm:my-auto m-auto">
       <ReverseEffectInfoItem
         title="تصویر"
         value={stats.images}
-        icon={<IoImage className="text-xl lg:text-3xl" />}
+        icon={<IoImage className="text-2xl lg:text-4xl" />}
         disable={!stats.images}
         className="basis-5/12 sm:basis-1/4"
       ></ReverseEffectInfoItem>
-      <div>
+      <div className={"hidden sm:block"}>
       <OctagonalDivider></OctagonalDivider>
       </div>
       <ReverseEffectInfoItem
         link={"/products/video"}
         title="ویدئو"
         value={stats.videos}
-        icon={<IoVideocam className="text-xl lg:text-3xl" />}
+        icon={<IoVideocam className="text-2xl lg:text-4xl" />}
         disable={!stats.videos}
         className="basis-5/12 sm:basis-1/4"
       ></ReverseEffectInfoItem>
