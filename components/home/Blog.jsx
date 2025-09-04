13e7611669d078dcdbbea3949ee51d8d95da287d
Parix <pariroox@gmail.com>
2025-09-04 14:14:08 +0330

    Refactor Blog component to utilize map function for rendering BlogCard components, enhancing code efficiency and maintainability

--- components/home/Blog.jsx
+++ components/home/Blog.jsx
@@ -3,16 +3,14 @@ import BlogCard from '@/components/BlogCard';
 import SectionTitleDivider from '../SectionTitleDivider';
 import Button from '@/components/Button';
 
-const Blog = ({data}) => {
+const Blog = ({ data }) => {
   return (
     <div className="pb-44 px-6 md:px-0">
       <SectionTitleDivider title="بلاگ"></SectionTitleDivider>
       <div className="grid sm:flex xl:grid grid-cols-1 xl:grid-cols-3 xl:grid-rows-5 gap-8 xl:container ltr xl:h-[56rem] h-[130rem] sm:h-[400px] overflow-auto hide-scrollbar">
-        <BlogCard className={`xl:col-span-2 xl:row-span-3 min-w-[350px]`} data={data[0]}></BlogCard>
-        <BlogCard className={`xl:col-span-1 xl:row-span-2 min-w-[350px]`} data={data[1]}></BlogCard>
-        <BlogCard className={`xl:col-span-1 xl:row-span-3 min-w-[350px]`} data={data[4]}></BlogCard>
-        <BlogCard className={`xl:col-span-1 xl:row-span-2 min-w-[350px]`} data={data[2]}></BlogCard>
-        <BlogCard className={`xl:col-span-1 xl:row-span-2 min-w-[350px]`} data={data[3]}></BlogCard>
+        {data.slice(0, 5).map((item, key) => (
+          <BlogCard key={key} className={`xl:col-span-2 xl:row-span-3 min-w-[350px]`} data={item}></BlogCard>
+        ))}
       </div>
       <Button
         className={"h-[4.6rem] w-52 rounded-3xl btn-circle mx-auto flex mt-20 text-[1.5rem] font-light btn-ghost"}
